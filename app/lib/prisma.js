6c86b5363e4f7c3501b030daf9367f09b830614a
Rik Verbeek <rvrbk.dev@gmail.com>
2024-10-10 19:26:07 +0200

    prisma

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis;
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'], // Optional: logs queries for debugging
+    log: ['query', 'info', 'warn', 'error'], // Optional: logs queries for debugging
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
