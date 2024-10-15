ce20388f451a85585af303446cf82c62252405f6
Rik Verbeek <rvrbk.dev@gmail.com>
2024-10-15 19:04:28 +0200

    ga4

--- app/layout.js
+++ app/layout.js
@@ -7,10 +7,10 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <GoogleAnalytics trackPageViews />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body>
-                <GoogleAnalytics trackPageViews />
                 {children}
             </body>
         </html>
