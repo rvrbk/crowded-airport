2d7c3deeeaa75d5315335407de093d59af9e0bd8
Rik Verbeek <rvrbk.dev@gmail.com>
2024-10-15 19:01:07 +0200

    ga3

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,18 @@
+"use client";
+
+import "./globals.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true); // Marks that the component has mounted on the client side
-    }, []);
-
     return (
         <html lang="en">
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body>
-                {isClient && <GoogleAnalytics trackPageViews />}
+                <GoogleAnalytics trackPageViews />
                 {children}
             </body>
         </html>
-    );
+  );
 }
